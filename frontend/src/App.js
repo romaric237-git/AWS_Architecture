411c4733a8987097317afa1d26e3da8bdd843c20
romaric237-git <romaricfakam@gmail.com>
2025-02-08 22:17:33 +0100

    Refactor: Déplacement de docker-compose.yml et modifications frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Chargement...");
+
+  useEffect(() => {
+    fetch("http://localhost:8000")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => setMessage("Erreur : " + err.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test Frontend ↔ Backend</h1>
+      <p>Message du backend : {message}</p>
     </div>
   );
 }
 
 export default App;
+
